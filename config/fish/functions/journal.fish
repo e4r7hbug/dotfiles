e75099cf9458f7e270a62b11e409b85841a190a5
Nate Tangsurat <ntangsurat@gogoair.com>
2016-05-18 15:16:20 -0500

    chore: Add descriptions to journal functions

--- config/fish/functions/journal.fish
+++ config/fish/functions/journal.fish
@@ -1,3 +1,3 @@
-function journal
+function journal -d "Quick journal of the day using Atom Beta"
   atom-beta "$HOME/git/gogo.bag/journal/"(date +%Y%m%d)".md"
 end
--- config/fish/functions/vj.fish
+++ config/fish/functions/vj.fish
@@ -1,3 +1,3 @@
-function vj
+function vj -d "Quick journal of the day using Vim"
   vim "$HOME/git/gogo.bag/journal/"(date "+%Y%m%d")".md"
 end
