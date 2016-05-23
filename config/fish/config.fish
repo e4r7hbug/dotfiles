13f2407508bfaca97ba7b93861266f36b9130afc
Nate Tangsurat <ntangsurat@gogoair.com>
2016-05-22 20:37:45 -0500

    chore: Remove omf in favour of fisherman

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,12 +1,3 @@
-# Path to Oh My Fish install.
-set -gx OMF_PATH "$HOME/.local/share/omf"
-
-# Customize Oh My Fish configuration path.
-#set -gx OMF_CONFIG "$HOME/.config/omf"
-
-# Load oh-my-fish configuration.
-source $OMF_PATH/init.fish
-
 fish_vi_mode
 
 for word in "$HOME/"{bin,.local/bin,Library/Python/2.7/bin,go/bin}
