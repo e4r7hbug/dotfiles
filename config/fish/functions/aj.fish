1fa52b30f1ec38be00800cc74be96de50956a883
Nate Tangsurat <e4r7hbug@gmail.com>
2016-08-02 11:30:21 -0500

    feat: Pull out aj and vj commands

---
+++ config/fish/functions/aj.fish
@@ -0,0 +1,4 @@
+function aj -d "Quick journal of the day using Atom"
+  journal
+  atom-beta "$journal_file"
+end
--- config/fish/functions/journal.fish
+++ config/fish/functions/journal.fish
@@ -1,3 +1,19 @@
-function journal -d "Quick journal of the day using Atom Beta"
-  atom-beta "$HOME/git/gogo.bag/journal/"(date +%Y%m%d)".md"
+function journal -d "Generate journal of the day file path"
+  set year (date "+%Y")
+  set month (date "+%m")
+  set date (date "+%Y%m%d")
+
+  set journal_dir "$HOME/git/gogo.bag/journal"
+  set year_dir "$journal_dir/$year"
+  set month_dir "$year_dir/$month"
+
+  set -g journal_file "$month_dir/$date.md"
+
+  if not test -d "$year_dir"
+    mkdir "$year_dir"
+
+    if not test -d "$month_dir"
+      mkdir -p "$month_dir"
+    end
+  end
 end
