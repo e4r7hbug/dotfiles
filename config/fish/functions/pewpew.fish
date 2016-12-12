52fcd1725c4c80210094a2c66d7b8136ff42202b
Nate Tangsurat <e4r7hbug@gmail.com>
2016-12-12 10:51:08 -0600

    fix: Include ansible environment

--- config/fish/functions/pewpew.fish
+++ config/fish/functions/pewpew.fish
@@ -1,7 +1,9 @@
 function pewpew
-    for env in {3,next}
-        for task in {wheel,install}
+    for task in {wheel,install}
+        for env in {3,next}
             pew in $env pip $task -r ~/.requirements.txt
         end
+
+        pew in ansible pip $task -r ~/.requirements-ansible.txt
     end
 end
