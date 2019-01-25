c4bc0b228e4e7e2fa0573cfb1bd2a0594c187b38
Nate Tangsurat <e4r7hbug@gmail.com>
2019-01-24 17:03:56 -0800

    fix: Use NeoVim

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,6 +4,7 @@ fish_vi_key_bindings
 set fish_user_paths "$HOME/"{.local/bin,.local/go/bin,go/bin,node_modules/.bin,.gem/ruby/*/bin,.cargo/bin}
 
 set -x GPG_TTY (tty)
+set -x EDITOR nvim
 
 set fish_complete_path "$HOME/git/github.fish-pip-completion" $fish_complete_path
 
