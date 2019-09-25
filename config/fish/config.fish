da0376095e15e7b00d7265a1f1342d8bb7dae447
Nate Tangsurat <e4r7hbug@gmail.com>
2019-09-25 14:39:25 -0700

    feat: fish set Python environment variables

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -3,8 +3,10 @@ fish_vi_key_bindings
 
 set fish_user_paths "$HOME/"{.local/bin,.local/go/bin,go/bin,node_modules/.bin,.cargo/bin} /usr/local/lib/ruby/gems/*/bin
 
-set -x GPG_TTY (tty)
 set -x EDITOR nvim
+set -x GPG_TTY (tty)
+set -x PIPENV_SKIP_LOCK True
+set -x PYTHONBREAKPOINT IPython.embed
 
 set fish_complete_path "$HOME/git/github.fish-pip-completion" $fish_complete_path
 
