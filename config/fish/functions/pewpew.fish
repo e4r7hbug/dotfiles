cc7ee7a5c5cef7ebfa4f065423e102586b2cc3e6
Nate Tangsurat <e4r7hbug@gmail.com>
2019-10-02 16:02:52 -0700

    chore: Remove the pew super virtual environment

--- config/fish/functions/pewpew.fish
+++
@@ -1,9 +0,0 @@
-function pewpew
-    for task in {wheel,install}
-        for env in {3,next}
-            pew in $env pip $task -r ~/.requirements.txt
-        end
-
-        pew in ansible pip $task -r ~/.requirements-ansible.txt
-    end
-end
